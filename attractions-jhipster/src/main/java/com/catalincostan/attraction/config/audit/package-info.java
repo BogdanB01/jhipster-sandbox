dfccbde7d5754df829548644ac9e3302ffae8e22
Catalin Costan <catalin.costan1@gmail.com>
2019-01-14 22:29:14 +0200

    Initial application generated by JHipster-5.7.2

---
+++ attractions-jhipster/src/main/java/com/catalincostan/attraction/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.catalincostan.attraction.config.audit;
---
+++ attractions-jhipster/src/main/java/com/catalincostan/attraction/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.catalincostan.attraction.web.rest;
